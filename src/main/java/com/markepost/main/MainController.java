6f48a5cf278e9aa79819a180ee31f2f7ef1b26a4
1J-Choi <chldnjswp110@naver.com>
2024-11-22 20:18:05 +0900

    레이아웃 헤더파일 구현

---
+++ src/main/java/com/markepost/main/MainController.java
@@ -0,0 +1,15 @@
+package com.markepost.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+	@GetMapping("/")
+	public String mainPage() {
+		return "main/main";
+	}
+}
