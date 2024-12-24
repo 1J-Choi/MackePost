2690b4e1fd77fba9e90f0ad7bd0238b8d30a342f
1J-Choi <chldnjswp110@naver.com>
2024-12-24 21:45:27 +0900

    swagger 정리 comment 까지

--- src/main/java/com/markepost/report/ReportRestController.java
+++ src/main/java/com/markepost/report/ReportRestController.java
@@ -12,20 +12,30 @@ import com.markepost.report.bo.ReportBO;
 import com.markepost.report.constant.ReportType;
 import com.markepost.report.domain.Report;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/report")
 @RequiredArgsConstructor
+@Tag(name = "Report API", description = "신고 관련 API")
 public class ReportRestController {
 	private final ReportBO reportBO;
 	
 	@PostMapping("/create")
+	@Operation(summary = "신고 등록", 
+	description = "신고하고자 하는 댓글or게시글의 id와 게시판id, 신고사유를 입력받아 등록합니다.")
 	public Map<String, Object> createReport(
+			@Parameter(description = "신고할 대상의 타입 (게시글, 댓글)")
 			@RequestParam("reportType") ReportType reportType, 
+			@Parameter(description = "신고할 대상(게시글, 댓글)의 ID")
 			@RequestParam("fkId") int fkId,
+			@Parameter(description = "신고할 대상이 있는 게시판 ID")
 			@RequestParam("boardId") int boardId,
+			@Parameter(description = "신고 사유")
 			@RequestParam("content") String content, 
 			HttpSession session) {
 		int userId = (int) session.getAttribute("userId");
